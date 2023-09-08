a29cba4be88578359025be0b8e34885e80f525f3
Evren AKGUN <evrenak@netas.com.tr>
2023-09-08 11:58:15 +0300

    Rework

--- src/test/java/cucumber/utilities/extentreports/ExtentManager.java
+++ src/test/java/cucumber/utilities/extentreports/ExtentManager.java
@@ -7,7 +7,7 @@ public class ExtentManager {
     public static final ExtentReports extentReports = new ExtentReports();
 
     public synchronized static ExtentReports createExtentReports() {
-        ExtentSparkReporter reporter = new ExtentSparkReporter("target/SparkReport/automation-report.html");
+        ExtentSparkReporter reporter = new ExtentSparkReporter("Reports/SparkReport/TurkcellPasajMobileTestAutomationReport.html");
         reporter.config().setReportName("Turkcell Pasaj Mobile Report");
         extentReports.attachReporter(reporter);
         extentReports.setSystemInfo("Turkcell", "Turkcell Pasaj Mobile");
